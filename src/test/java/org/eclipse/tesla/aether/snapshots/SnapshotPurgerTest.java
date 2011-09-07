9e928d23c6851a359615f8460bdbca04785284a1
Benjamin Bentmann <benjamin.bentmann@udo.edu>
2011-09-08 00:41:49 +0200

    o Added some unit tests

---
+++ src/test/java/org/eclipse/tesla/aether/snapshots/SnapshotPurgerTest.java
@@ -0,0 +1,248 @@
+package org.eclipse.tesla.aether.snapshots;
+
+/*******************************************************************************
+ * Copyright (c) 2011 Sonatype, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ *   http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.junit.rules.TestName;
+import org.sonatype.aether.RepositoryEvent;
+import org.sonatype.aether.artifact.Artifact;
+import org.sonatype.aether.impl.internal.SimpleLocalRepositoryManager;
+import org.sonatype.aether.repository.LocalRepositoryManager;
+import org.sonatype.aether.repository.RemoteRepository;
+import org.sonatype.aether.test.impl.SysoutLogger;
+import org.sonatype.aether.transfer.ArtifactNotFoundException;
+import org.sonatype.aether.util.DefaultRepositorySystemSession;
+import org.sonatype.aether.util.artifact.DefaultArtifact;
+import org.sonatype.aether.util.listener.DefaultRepositoryEvent;
+
+/**
+ */
+public class SnapshotPurgerTest
+{
+
+    enum Files
+    {
+        ARTIFACT_ONLY, ARTIFACT_AND_AUXILIARIES
+    }
+
+    private static final String GROUP_ID = "test";
+
+    private static final String ARTIFACT_ID = "test";
+
+    private static final String[] SNAPSHOTS = { "1.0-20110907.162759-1", "1.0-20110907.182759-23",
+        "1.0-20110907.232759-234", "1.0-20110908.002759-1234" };
+
+    @Rule
+    public TestName testName = new TestName();
+
+    @Rule
+    public TemporaryFolder localRepoDir = new TemporaryFolder();
+
+    private SnapshotPurger purger;
+
+    private DefaultRepositorySystemSession session;
+
+    @Before
+    public void setUp()
+    {
+        System.out.println( "========== " + testName.getMethodName() );
+
+        purger = new SnapshotPurger();
+        purger.setLogger( new SysoutLogger() );
+
+        SimpleLocalRepositoryManager lrm = new SimpleLocalRepositoryManager( localRepoDir.getRoot() );
+        session = new DefaultRepositorySystemSession().setLocalRepositoryManager( lrm );
+    }
+
+    @After
+    public void tearDown()
+    {
+        purger = null;
+        session = null;
+    }
+
+    private void assertExistent( File... files )
+    {
+        for ( File file : files )
+        {
+            assertTrue( "Missing " + file.getAbsolutePath(), file.isFile() );
+        }
+    }
+
+    private void assertAbsent( File... files )
+    {
+        for ( File file : files )
+        {
+            assertFalse( "Existent " + file.getAbsolutePath(), file.exists() );
+        }
+    }
+
+    private Artifact newArtifact( String ext, String cls, String version )
+    {
+        Artifact artifact = new DefaultArtifact( GROUP_ID, ARTIFACT_ID, cls, ext, version );
+
+        RemoteRepository repo = new RemoteRepository( "test", "default", "file:" );
+        LocalRepositoryManager lrm = session.getLocalRepositoryManager();
+        File file = new File( lrm.getRepository().getBasedir(), lrm.getPathForRemoteArtifact( artifact, repo, "" ) );
+        artifact = artifact.setFile( file );
+
+        return artifact;
+    }
+
+    private Artifact[] newArtifacts( String ext, String cls, String... versions )
+    {
+        Artifact[] artifacts = new Artifact[versions.length];
+        for ( int i = 0; i < versions.length; i++ )
+        {
+            artifacts[i] = newArtifact( ext, cls, versions[i] );
+        }
+        return artifacts;
+    }
+
+    private File[] createFiles( Files fileSet, Artifact... artifacts )
+        throws IOException
+    {
+        int filesPerArtifact = fileSet == Files.ARTIFACT_AND_AUXILIARIES ? 4 : 1;
+
+        File[] files = new File[artifacts.length * filesPerArtifact];
+
+        for ( int i = 0; i < artifacts.length; i++ )
+        {
+            Artifact artifact = artifacts[i];
+
+            int index = i * filesPerArtifact;
+
+            // artifact file
+            files[index + 0] = artifact.getFile();
+            files[index + 0].getAbsoluteFile().getParentFile().mkdirs();
+            files[index + 0].createNewFile();
+
+            if ( fileSet == Files.ARTIFACT_AND_AUXILIARIES )
+            {
+                // checksum files
+                files[index + 1] = new File( files[index + 0].getAbsolutePath() + ".md5" );
+                files[index + 1].createNewFile();
+                files[index + 2] = new File( files[index + 0].getAbsolutePath() + ".sha1" );
+                files[index + 2].createNewFile();
+
+                // resolution error status files
+                files[index + 3] = new File( files[index + 0].getAbsolutePath() + ".lastUpdated" );
+                files[index + 3].createNewFile();
+            }
+        }
+
+        return files;
+    }
+
+    private RepositoryEvent newEvent( Artifact artifact )
+    {
+        DefaultRepositoryEvent event =
+            new DefaultRepositoryEvent( RepositoryEvent.EventType.ARTIFACT_DOWNLOADED, session );
+        event.setFile( artifact.getFile() );
+        event.setArtifact( artifact );
+        if ( event.getFile() == null )
+        {
+            event.setException( new ArtifactNotFoundException( artifact, null ) );
+        }
+
+        return event;
+    }
+
+    private void notify( Artifact artifact )
+    {
+        purger.artifactDownloaded( newEvent( artifact ) );
+    }
+
+    @Test
+    public void testDoNotCrashWhenEventHasNoFileDueToDownloadFailure()
+    {
+        notify( newArtifact( "jar", "", SNAPSHOTS[0] ).setFile( null ) );
+    }
+
+    @Test
+    public void testDoNotDeleteCurrentSnapshot()
+        throws Exception
+    {
+        Artifact artifact = newArtifact( "jar", "", SNAPSHOTS[0] );
+        File[] files = createFiles( Files.ARTIFACT_AND_AUXILIARIES, artifact );
+        notify( artifact );
+        assertExistent( files );
+    }
+
+    @Test
+    public void testDeleteOldSnapshots()
+        throws Exception
+    {
+        Artifact newArtifact = newArtifact( "jar", "", SNAPSHOTS[3] );
+        Artifact[] oldArtifacts = newArtifacts( "jar", "", SNAPSHOTS[0], SNAPSHOTS[1], SNAPSHOTS[2] );
+        File[] newFiles = createFiles( Files.ARTIFACT_ONLY, newArtifact );
+        File[] oldFiles = createFiles( Files.ARTIFACT_ONLY, oldArtifacts );
+        notify( newArtifact );
+        assertExistent( newFiles );
+        assertAbsent( oldFiles );
+    }
+
+    @Test
+    public void testDeleteAuxiliaryFilesOfOldSnapshotsAsWell()
+        throws Exception
+    {
+        Artifact newArtifact = newArtifact( "jar", "", SNAPSHOTS[3] );
+        Artifact[] oldArtifacts = newArtifacts( "jar", "", SNAPSHOTS[0], SNAPSHOTS[1], SNAPSHOTS[2] );
+        File[] newFiles = createFiles( Files.ARTIFACT_ONLY, newArtifact );
+        File[] oldFiles = createFiles( Files.ARTIFACT_AND_AUXILIARIES, oldArtifacts );
+        notify( newArtifact );
+        assertExistent( newFiles );
+        assertAbsent( oldFiles );
+    }
+
+    @Test
+    public void testDoNotDeleteSnapshotsOfOtherArtifacts()
+        throws Exception
+    {
+        Artifact artifact = newArtifact( "jar", "", SNAPSHOTS[1] );
+        Artifact artifact1 = newArtifact( "pom", "", SNAPSHOTS[0] );
+        Artifact artifact2 = newArtifact( "jar", "sources", SNAPSHOTS[0] );
+        Artifact artifact3 = newArtifact( "jar", "javadoc", SNAPSHOTS[0] );
+        File[] newFiles = createFiles( Files.ARTIFACT_ONLY, artifact );
+        File[] oldFiles = createFiles( Files.ARTIFACT_AND_AUXILIARIES, artifact1, artifact2, artifact3 );
+        notify( artifact );
+        assertExistent( newFiles );
+        assertExistent( oldFiles );
+    }
+
+    @Test
+    public void testDoNotChokeUponFailureToDeleteOldSnapshot()
+        throws Exception
+    {
+        Artifact newArtifact = newArtifact( "jar", "", SNAPSHOTS[1] );
+        Artifact oldArtifact = newArtifact( "jar", "", SNAPSHOTS[0] );
+        createFiles( Files.ARTIFACT_ONLY, newArtifact );
+        File[] oldFiles = createFiles( Files.ARTIFACT_ONLY, oldArtifact );
+        RandomAccessFile raf = new RandomAccessFile( oldFiles[0], "rw" );
+        try
+        {
+            notify( newArtifact );
+        }
+        finally
+        {
+            raf.close();
+        }
+    }
+
+}
